b1d297dfbc6a42997ff3051a7cf72ba817d51b44
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2023-05-09 23:02:11 +0300

    (add): user id validation

--- orange_hackathon_flutter/lib/views/screens/bills/electricity/electricity_bill_user_id_screen.dart
+++ orange_hackathon_flutter/lib/views/screens/bills/electricity/electricity_bill_user_id_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:orange_hackathon_flutter/views/screens/bills/electricity/electricity_bill_amount_screen.dart';
 import 'package:orange_hackathon_flutter/views/widgets/default_button.dart';
 
+// ignore: must_be_immutable
 class ElectricityBillUserIDScreen extends StatelessWidget {
   late String companyName;
 
   ElectricityBillUserIDScreen({super.key, required this.companyName});
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -34,49 +37,64 @@ class ElectricityBillUserIDScreen extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          children: [
-            const SizedBox(height: 30),
-            Container(
-              height: 48,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18.0),
-                color: HexColor("#EFEFEF"),
-              ),
-              child: TextFormField(
-                cursorColor: Colors.transparent,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10.4, horizontal: 16),
-                    border: InputBorder.none,
-                    hintText: 'User ID',
-                    hintStyle: TextStyle(
-                        fontFamily: 'poppins',
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                        color: HexColor("#939094"))),
-              ),
-            ),
-            const SizedBox(height: 48),
-            Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                ),
-                child: SizedBox(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                Container(
                   height: 48,
                   width: double.infinity,
-                  child: DefaultButton(
-                      onSubmitted: () {},
-                      color: HexColor("#333E96"),
-                      text: "Continue",
-                      textColor: HexColor("#F7F7F7")),
-                )),
-          ],
-        ),
-      ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18.0),
+                    color: HexColor("#EFEFEF"),
+                  ),
+                  child: TextFormField(
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please enter your ID';
+                      }
+                    },
+                    cursorColor: Colors.transparent,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10.4, horizontal: 16),
+                        border: InputBorder.none,
+                        hintText: 'User ID',
+                        hintStyle: TextStyle(
+                            fontFamily: 'poppins',
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: HexColor("#939094"))),
+                  ),
+                ),
+                const SizedBox(height: 48),
+                Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                    ),
+                    child: SizedBox(
+                      height: 48,
+                      width: double.infinity,
+                      child: DefaultButton(
+                          onSubmitted: () {
+                            if (formKey.currentState!.validate()) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          ElectricityBillsAmountScreen()));
+                            }
+                          },
+                          color: HexColor("#333E96"),
+                          text: "Continue",
+                          textColor: HexColor("#F7F7F7")),
+                    )),
+              ],
+            ),
+          )),
     );
   }
 }
